767609725326f9f29a68f14c82b36e3e57762aba
AliceGonzalez <alicegcodes@gmail.com>
2024-03-14 20:49:34 -0400

    Passed test : CatHouseTest

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -2,6 +2,7 @@ package rocks.zipcodewilmington;
 
 import org.junit.Assert;
 import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
 import rocks.zipcodewilmington.animals.Dog;
 import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
 
@@ -14,18 +15,22 @@ public class AnimalFactoryTest {
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
     @Test
     public void createDogTest(){
+        String givenName = "Muffin";
+        Date givenDate = new Date("3/4/1997");
+        Dog dog = AnimalFactory.createDog(givenName, givenDate);
 
-//
-//        Dog dog = new Dog ("Robby",new Date(),null);
-//
-//
-//
-//        String newName = dog.createDog();
-//        Date newDate = creat
-
+        Assert.assertEquals(givenName,dog.getName());
+        Assert.assertEquals(givenDate,dog.getBirthDate());
     }
 
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
     @Test
-    public void createCatTest(){}
+    public void createCatTest(){
+        String givenName = "Cupcake";
+        Date givenDate = new Date("1/4/2000");
+        Cat cat = AnimalFactory.createCat(givenName, givenDate);
+
+        Assert.assertEquals(givenName,cat.getName());
+        Assert.assertEquals(givenDate,cat.getBirthDate());
+    }
 }
--- src/test/java/rocks/zipcodewilmington/CatHouseTest.java
+++ src/test/java/rocks/zipcodewilmington/CatHouseTest.java
@@ -1,15 +1,76 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.animal_storage.CatHouse;
+
+import java.util.Date;
+
 /**
  * @author leon on 4/19/18.
  */
 public class CatHouseTest {
     // TODO - Create tests for `void add(Cat cat)`
+    @Test
     public void addTest(){
+        Integer catID = 372;
+        Cat cat = new Cat(null, null, catID);
+
+        CatHouse.add(cat);
+        Cat actualCat = CatHouse.getCatById(catID);
+
+        Assert.assertEquals(catID, actualCat.getId());
+    }
+
+    // TODO - Create tests for `void remove(Integer id)`
+    @Test
+    public void removeCatByIDTest(){
+        Cat cat = new Cat(null, null, 736);
+        Integer expectedCatID = null;
+
+        CatHouse.add(cat);
+        CatHouse.remove(cat.getId());
+        Cat actualCat = CatHouse.getCatById(cat.getId());
+
+        Assert.assertEquals(expectedCatID, actualCat);
+    }
+    // TODO - Create tests for `void remove(Cat cat)`
+    @Test
+    public void removeCatTest(){
+        Cat cat = new Cat("Lola", new Date("6/1/2003"), 236);
+
+        CatHouse.add(cat);
+        CatHouse.remove(cat);
+        Cat actualCat = CatHouse.getCatById(236);
+
+        Assert.assertEquals(null, actualCat);
+    }
+    // TODO - Create tests for `Cat getCatById(Integer id)`
+    @Test
+    public void gatCatByIDTest(){
+        Cat expectedCat = new Cat("Dave", new Date("3/1/1990"), 723);
+
+        CatHouse.add(expectedCat);
+        Cat actualCat = CatHouse.getCatById(723);
+
+        Assert.assertEquals(expectedCat, actualCat);
+    }
+    // TODO - Create tests for `Integer getNumberOfCats()`
+    @Test
+    public void getNumberOfCatsTest(){
+        Cat cat1 = new Cat("Milo", new Date("6/2/2020"),342);
+        Cat cat2 = new Cat("Eric", new Date("4/1/1998"),911);
+        Cat cat3 = new Cat("Muffin", new Date("6/12/1998"),123);
+        Integer expectedNum = 3;
+
+
+        CatHouse.add(cat1);
+        CatHouse.add(cat2);
+        CatHouse.add(cat3);
+        Integer numOfCats = CatHouse.getNumberOfCats();
+
+        Assert.assertEquals(expectedNum,numOfCats);
 
     }
-    // TODO - Create tests for `void remove(Integer id)`
-    // TODO - Create tests for `void remove(Cat cat)`
-    // TODO - Create tests for `Cat getCatById(Integer id)`
-    // TODO - Create tests for `Integer getNumberOfCats()`
 }
